9bad8ec58932f291883914d9c14876976eda2c0b
Lokious <mary2001@hp2021.localdomain>
2022-07-10 17:46:43 +0200

    upload data

--- model.py
+++ model.py
@@ -1,20 +1,9 @@
-
+#!/usr/bin/env python3
 import pandas as pd
-import openpyxl
 from sys import argv
-def readrhlist():
-    # rheauniprot = open("C:/Users/HP/Desktop/thesis/data/rhea2uniprot_sprot.tsv").readlines()
-    # for line in rheauniprot:
-    #     line_list = (line.strip()).split("\t")
-    #     RHEA_ID = line_list[0]
-    #     DIRECTION = line_list[1]
-    #     uniprotid = line_list[3]
-    #     if line_list[0].startswith("R"):
-    #         rheauniprot_dataframe = pd.DataFrame(columns=[line_list[0], line_list[1]])
-    #     else:
-    #         new_row = {"RHEA_ID":RHEA_ID,"DIRECTION":DIRECTION}
-    #         rheauniprot_dataframe = pd.concat([rheauniprot_dataframe, pd.DataFrame(new_row, index=[uniprotid])])
-    rheauniprot_dataframe = pd.read_csv("C:/Users/HP/Desktop/thesis/data/rhea2uniprot_sprot.tsv", sep='\t', header=0, index_col=3)
+def readrhlist(file_name):
+
+    rheauniprot_dataframe = pd.read_table(file_name, header=0, index_col=3,sep='\t', encoding="ISO-8859-1")
     rheauniprot_dataframe = rheauniprot_dataframe.rename_axis(index='Entry')
     return rheauniprot_dataframe
 
@@ -26,10 +15,12 @@ def read_sequence(file):
 
 def main():
     # readfile whihc contains the rhea id and related uniprotid
-    rheauniprot_dataframe = readrhlist()
+    rh_file = argv[1]
+    rheauniprot_dataframe = readrhlist(rh_file)
     print(rheauniprot_dataframe)
     #read id and sequences in dataframe
-    id_seq_dataframe = read_sequence("C:/Users/HP/Desktop/thesis/data/id_tosequence.xlsx")
+    seq_file = argv[2]
+    id_seq_dataframe = read_sequence(seq_file)
     print(id_seq_dataframe)
     #link the sequences and reaction participant put in one csv file
     fulldata = id_seq_dataframe.join(rheauniprot_dataframe)
@@ -37,7 +28,9 @@ def main():
     # groups_uniprotid = fulldata.groupby(["Entry"])
     # for group in groups_uniprotid:
     #     print(group)
-    fulldata.to_csv("C:/Users/HP/Desktop/thesis/data/fulldata.csv")
+    #save to file
+    file_name = input("please input file name and directory")
+    fulldata.to_csv(file_name)
 
 if __name__ == "__main__":
     main()
