58f35779a9393cf2847c28c187ed2b51385350f8
Lokious <mary2001@hp2021.localdomain>
2022-07-14 09:46:15 +0200

    change some script

--- model.py
+++ model.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
+"""
+Author:         Yingjie Shao
+Description:
+Dependencies:   Python3.9
+                numpy
+                pandas
+datafile should be put in the directory data/
+includes Rhea-ec_2_1_1.tsv
+"""
+
 import pandas as pd
 from sys import argv
+def target_sequences(file):
+    """
+    This function is used to return Uniprot entry of target sequences from hmmscan
+    :param file:string, file name
+    :return: hmmscan_df: dataframe, include all the Uniprot entry from hmmscan output
+    """
+    hmmscan_df = pd.read_table(file, header=None, comment='#',sep= '\s+')
+    hmmscan_df = hmmscan_df.iloc[:,2]
+    for i in list(hmmscan_df.index):
+        hmmscan_df.iloc[i] = (hmmscan_df.iloc[i]).split("|")[1]
+    return hmmscan_df
 
 def readrhlist(file_name):
 
@@ -13,51 +34,64 @@ def read_sequence(file):
     lines = pd.read_excel(file,index_col=0)
     return lines
 
+
 def rheaid_touniprot(rhea,data_frame):
 
     entrys = []
     for id in rhea:
 
         number = (id.strip()).split(":")[-1]
-
         uniprot_entry = data_frame[data_frame['RHEA_ID'] == int(number)]
         if uniprot_entry.empty:
             entrys.append("NA")
         else:
-
             entrys.append(uniprot_entry.iloc[:,0])
     return entrys
+
+
 def get_substrate_inchkey():
     """
     This function is used to split the substrate and product and save in dataframe
-    :parameter
+    :parameter:None
 
-    :return: datframe which use rhea as index
+    :return:
+    rhea_dataframe: datframe, which use rhea as index product and substrate in
+    separate columns
+
     """
     #read file includes rhea id and chEBI id and name
     rhea_dataframe = pd.read_table("data/Rhea-ec_2_1_1.tsv", header=0, index_col=0,
                                           sep='\t')
     rhea_dataframe["substrate"] = 0
+    rhea_dataframe["product"] = 0
     for id in list(rhea_dataframe.index):
         equation = rhea_dataframe.loc[id,"Equation"]
         subtrates = equation.split("=")[0]
-        subtrate_list = subtrates.split("+")
-        name = (rhea_dataframe.loc[id,"ChEBI name"]).split(";")
+        product = equation.split("=")[1]
+        #split by " + ", otherwise H(+) will be sepreate into two part
+        subtrate_list = subtrates.split(" + ")
+        product_list = product.split(" + ")
         chebiid = (rhea_dataframe.loc[id,"ChEBI identifier"]).split(";")
         participentid = (rhea_dataframe.loc[id,"Participant identifier"]).split(";")
-        subtrate_dict_1 = dict(zip(name,chebiid))
-        subtrate_dict_2 = dict(zip(name, participentid))
-        print(subtrate_dict_1)
-        sub = ""
-        for item in subtrate_list:
-            if item.strip(" ") in subtrate_dict_1:
-                sub += subtrate_dict_1[item.strip(" ")]+";"
-            else:
-                #error may occur because some of the substrate is not exact
-                sub += "error;"
+        sub_rh = ""
+        sub_ch = ""
+        pro_rh = ""
+        pro_ch = ""
 
-        rhea_dataframe.loc[id,"substrate"] = sub
-    print(rhea_dataframe)
+        for index,item in enumerate(subtrate_list):
+            sub_rh += participentid[index]+";"
+            sub_ch += chebiid[index]+";"
+        for i in range(len(product_list),0,-1):
+            pro_rh += participentid[-i]+";"
+            pro_ch += chebiid[-i]+";"
+
+        rhea_dataframe.loc[id,"substrate_RHEA-COMP"] = sub_rh
+        rhea_dataframe.loc[id, "substrate_CHEBI"] = sub_ch
+        rhea_dataframe.loc[id,"product_RHEA-COMP"] = pro_rh
+        rhea_dataframe.loc[id, "product_CHEBI"] = pro_ch
+
+    return rhea_dataframe
+
 
 def main():
     # readfile whihc contains the rhea id and related uniprotid
@@ -67,7 +101,6 @@ def main():
     rh_file = "data/rhea2uniprot_sprot.tsv"
     rheauniprot_dataframe = readrhlist(rh_file)
 
-    #print(rheauniprot_dataframe)
     #read id and sequences in dataframe
     # run with pycharm
     seq_file = "data/id_tosequence.xlsx"
@@ -96,5 +129,7 @@ def main():
     # print(uniprot_list)
 
     get_substrate_inchkey()
+
+    target_sequences("E:/Download/regioselectivity_prediction/data/hmm_out/top_ten_hits_exclude_nomethylrelated/hmmscantbout_top_6_hit.pfam")
 if __name__ == "__main__":
     main()
