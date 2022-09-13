54e8b690da0643ef4ac5047299072cfd5a775add
Lokious <mary2001@hp2021.localdomain>
2022-09-13 15:36:43 +0200

    add function for getting amino acides which are close to the active sites

--- script/sequence.py
+++ script/sequence.py
@@ -10,7 +10,7 @@ This code is to parse sequence data
 import sys
 import unittest
 import pandas as pd
-from Bio import SeqIO
+from Bio import AlignIO, SeqIO
 from os import path
 class sequences():
 
@@ -91,11 +91,85 @@ class sequences():
                     # print(record)
                     # print(record_rename)
 
+    def get_sites_from_alignment(self,format="fasta",file=""):
+        """
 
+
+        :param format:
+        :param file:
+        :return:
+
+
+        """
+        align = AlignIO.read(file, "fasta")
+
+    def get_AA_within_distance_from_structure_file(self,file="../autodata/align/align_seed_sequences_with_structure/3rod.pdb",residue_dictionary:dict={11:"Y"}):
+        """
+        This function is to get the id of residues close to active sites
+
+        :param file: string, path and name of pdb file
+        :param residue_dictionary: {id of active site:only letter code of AA}
+        :return:
+        amino_acide_close_to_active_site: dictionary, id of active site is key,
+        value is a tuple which contains the id, name and distance of close AA
+        For example:
+        {11: [(8, 'LYS', 5.5259695), (10, 'THR', 3.8049822), (11, 'TYR', 0.0)}
+
+
+        Some related problem: https://www.biostars.org/p/401105/
+        """
+        from Bio.PDB import PDBParser
+        from Bio.Data.IUPACData import protein_letters_1to3
+        # create parser
+        parser = PDBParser()
+
+        # read structure from file
+
+        structure = parser.get_structure("pdb_structure",file)
+
+        model = structure[0]
+        chain = model['A']
+        residues=list(chain.get_residues())
+        active_sites=[]
+        for id in residue_dictionary.keys():
+            amino_acid= residue_dictionary[id]
+            print(protein_letters_1to3[amino_acid])
+            print(chain[id].get_resname())
+            if protein_letters_1to3[amino_acid].upper() == chain[id].get_resname().upper():
+                active_sites.append(chain[id])
+        amino_acide_close_to_active_site={}
+        for id1,residue1 in enumerate(active_sites):
+            #remove residue from the list of residues, so it only count the distance between two residue once
+
+            print("id1:{}".format(id1+1))
+            #seems residue.get_id()[1] will return the id from author, which is
+            #use in the paper
+            print(residue1.get_id()[1])
+            print(residue1.get_resname())
+            for id2,residue2 in enumerate(residues):
+                # compute distance between alpha C atoms
+                print("id2:{}".format(id2+id1+1))
+                print(residue2.get_id()[1])
+                print(residue2.get_resname())
+                try:
+                    distance = residue1['CA'] - residue2['CA']
+                except KeyError:
+                    ## no CA atom
+                    continue
+                if distance < 6:
+
+                    if residue1.get_id()[1] not in amino_acide_close_to_active_site:
+                        amino_acide_close_to_active_site[residue1.get_id()[1]]=[(residue2.get_id()[1],residue2.get_resname(),distance)]
+                    else:
+                        amino_acide_close_to_active_site[residue1.get_id()[1]].append((residue2.get_id()[1],residue2.get_resname(),distance))
+        print(amino_acide_close_to_active_site)
+        return amino_acide_close_to_active_site
+    
 def main():
     #unittest.main()
 
     seq=sequences()
+    seq.get_AA_within_distance_from_structure_file()
 
     #seq.group_seq_based_on_methylated_type(inputfile="../autodata/seq_smiles_all.csv",save_directory="../autodata/sequences")
     #seq.remove_duplicate()
